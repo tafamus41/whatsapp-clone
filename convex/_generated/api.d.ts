31465cd524c562e94f669779d55eef71629d1019
asimov <seyranmustafa4@gmail.com>
2025-04-04 20:57:44 +0300

    p

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as clerk from "../clerk.js";
 import type * as http from "../http.js";
 import type * as messages from "../messages.js";
 import type * as users from "../users.js";
@@ -26,6 +27,7 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  clerk: typeof clerk;
   http: typeof http;
   messages: typeof messages;
   users: typeof users;
