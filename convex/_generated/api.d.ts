b4fa3491cd920069509f43fea409eab89d62f693
asimov <seyranmustafa4@gmail.com>
2025-04-04 23:01:24 +0300

    p

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -16,8 +16,6 @@ import type {
 import type * as clerk from "../clerk.js";
 import type * as conversations from "../conversations.js";
 import type * as http from "../http.js";
-import type * as messages from "../messages.js";
-import type * as openai from "../openai.js";
 import type * as users from "../users.js";
 
 /**
@@ -32,8 +30,6 @@ declare const fullApi: ApiFromModules<{
   clerk: typeof clerk;
   conversations: typeof conversations;
   http: typeof http;
-  messages: typeof messages;
-  openai: typeof openai;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
